b64a85310d88441954f418a22715178329dd7b16
Ricardo <131534712+RicardoV1e1r4@users.noreply.github.com>
2026-04-23 19:25:57 -0300

    Edição 1
    Foram feitas algumas edições apenas no esquema

--- experiments/AM Modulation/DSSC_SC.py
+++ experiments/AM Modulation/DSSC_SC.py
@@ -28,7 +28,7 @@ import threading
 
 
 
-class DSBC_SC(gr.top_block, Qt.QWidget):
+class DSSC_SC(gr.top_block, Qt.QWidget):
 
     def __init__(self):
         gr.top_block.__init__(self, "Not titled yet", catch_exceptions=True)
@@ -51,7 +51,7 @@ class DSBC_SC(gr.top_block, Qt.QWidget):
         self.top_grid_layout = Qt.QGridLayout()
         self.top_layout.addLayout(self.top_grid_layout)
 
-        self.settings = Qt.QSettings("gnuradio/flowgraphs", "DSBC_SC")
+        self.settings = Qt.QSettings("gnuradio/flowgraphs", "DSSC_SC")
 
         try:
             geometry = self.settings.value("geometry")
@@ -64,7 +64,6 @@ class DSBC_SC(gr.top_block, Qt.QWidget):
         ##################################################
         # Variables
         ##################################################
-        self.samp_rate_audio = samp_rate_audio = 240e3
         self.samp_rate = samp_rate = 48000
 
         ##################################################
@@ -73,7 +72,7 @@ class DSBC_SC(gr.top_block, Qt.QWidget):
 
         self.qtgui_time_sink_x_0 = qtgui.time_sink_f(
             1024, #size
-            samp_rate_audio, #samp_rate
+            samp_rate, #samp_rate
             "", #name
             2, #number of inputs
             None # parent
@@ -123,7 +122,7 @@ class DSBC_SC(gr.top_block, Qt.QWidget):
             1024, #size
             window.WIN_BLACKMAN_hARRIS, #wintype
             0, #fc
-            samp_rate_audio, #bw
+            samp_rate, #bw
             "", #name
             1,
             None # parent
@@ -163,61 +162,53 @@ class DSBC_SC(gr.top_block, Qt.QWidget):
         self._qtgui_freq_sink_x_0_win = sip.wrapinstance(self.qtgui_freq_sink_x_0.qwidget(), Qt.QWidget)
         self.top_layout.addWidget(self._qtgui_freq_sink_x_0_win)
         self.blocks_wavfile_sink_0 = blocks.wavfile_sink(
-            'D:\\Documentos\\Faculdade\\IC_Radios_Cognitivos\\Sinal_e_Audio_do_GNURadio\\AM_Signal',
+            'D:\\Documentos\\Faculdade\\IC - Telecomunicacoes\\Sinal Modulado\\AM_Signal',
             1,
             samp_rate,
             blocks.FORMAT_WAV,
             blocks.FORMAT_PCM_32,
             False
             )
-        self.blocks_throttle2_0 = blocks.throttle( gr.sizeof_float*1, samp_rate_audio, True, 0 if "auto" == "auto" else max( int(float(0.1) * samp_rate_audio) if "auto" == "time" else int(0.1), 1) )
-        self.blocks_repeat_0 = blocks.repeat(gr.sizeof_float*1, 5)
+        self.blocks_throttle2_0 = blocks.throttle( gr.sizeof_float*1, samp_rate, True, 0 if "auto" == "auto" else max( int(float(0.1) * samp_rate) if "auto" == "time" else int(0.1), 1) )
         self.blocks_multiply_xx_0 = blocks.multiply_vff(1)
         self.audio_source_0 = audio.source(samp_rate, '', False)
-        self.analog_sig_source_x_0 = analog.sig_source_f(samp_rate_audio, analog.GR_COS_WAVE, 1e5, 1, 0, 0)
+        self.analog_sig_source_x_0 = analog.sig_source_f(samp_rate, analog.GR_COS_WAVE, 1e6, 1, 0, 0)
 
 
         ##################################################
         # Connections
         ##################################################
         self.connect((self.analog_sig_source_x_0, 0), (self.blocks_multiply_xx_0, 1))
-        self.connect((self.audio_source_0, 0), (self.blocks_repeat_0, 0))
+        self.connect((self.audio_source_0, 0), (self.blocks_multiply_xx_0, 0))
+        self.connect((self.audio_source_0, 0), (self.qtgui_time_sink_x_0, 1))
         self.connect((self.blocks_multiply_xx_0, 0), (self.blocks_throttle2_0, 0))
-        self.connect((self.blocks_repeat_0, 0), (self.blocks_multiply_xx_0, 0))
-        self.connect((self.blocks_repeat_0, 0), (self.qtgui_time_sink_x_0, 1))
         self.connect((self.blocks_throttle2_0, 0), (self.blocks_wavfile_sink_0, 0))
         self.connect((self.blocks_throttle2_0, 0), (self.qtgui_freq_sink_x_0, 0))
         self.connect((self.blocks_throttle2_0, 0), (self.qtgui_time_sink_x_0, 0))
 
 
     def closeEvent(self, event):
-        self.settings = Qt.QSettings("gnuradio/flowgraphs", "DSBC_SC")
+        self.settings = Qt.QSettings("gnuradio/flowgraphs", "DSSC_SC")
         self.settings.setValue("geometry", self.saveGeometry())
         self.stop()
         self.wait()
 
         event.accept()
 
-    def get_samp_rate_audio(self):
-        return self.samp_rate_audio
-
-    def set_samp_rate_audio(self, samp_rate_audio):
-        self.samp_rate_audio = samp_rate_audio
-        self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate_audio)
-        self.blocks_throttle2_0.set_sample_rate(self.samp_rate_audio)
-        self.qtgui_freq_sink_x_0.set_frequency_range(0, self.samp_rate_audio)
-        self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate_audio)
-
     def get_samp_rate(self):
         return self.samp_rate
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
+        self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
+        self.blocks_throttle2_0.set_sample_rate(self.samp_rate)
+        self.qtgui_freq_sink_x_0.set_frequency_range(0, self.samp_rate)
+        self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate)
 
 
 
 
-def main(top_block_cls=DSBC_SC, options=None):
+def main(top_block_cls=DSSC_SC, options=None):
 
     qapp = Qt.QApplication(sys.argv)
 
